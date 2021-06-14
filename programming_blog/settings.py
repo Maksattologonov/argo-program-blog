935bb927eb98c54ec838d5e6a33797db2ba014e9
Maksattolgonov <you@example.com>
2021-06-14 15:08:40 +0600

    fix

--- programming_blog/settings.py
+++ programming_blog/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'programming_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "programblog",
+        'NAME': 'programblog',
         'USER': 'myprojectuser',
         'PASSWORD': "password",
         'HOST': '127.0.0.1',
