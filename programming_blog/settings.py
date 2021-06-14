11b169130ea176c30d69fa481e2a495337ae0898
Maksattolgonov <you@example.com>
2021-06-14 13:30:06 +0600

    delete requirements

--- programming_blog/settings.py
+++ programming_blog/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['agro-program-blog.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
